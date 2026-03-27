cfb92b4cf5a4bf2806fadc8a24f6d65271bf0e2d
Clifford A <canyeaws@gmail.com>
2026-03-27 02:05:18 -0500

    updated files

--- Terraform/vpc/main.tf
+++ Terraform/vpc/main.tf
@@ -348,7 +348,7 @@ resource "aws_lambda_function" "cpennymac" {
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.12"
 
-  filename         = "${path.module}/lambda/clean/lambda_function.zip"
+  filename         = "${path.module}/lambda/cleanup/lambda_function.zip"
   source_code_hash = filebase64sha256("${path.module}/lambda/cleanup/lambda_function.zip")
 
   timeout = 60
